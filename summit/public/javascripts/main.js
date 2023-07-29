f2d6e5bc96da9174878ecebeaf853cabfc5681de
jek01680 <jek01680@icloud.com>
2023-07-30 00:50:05 +0900

    first commit

---
+++ summit/public/javascripts/main.js
@@ -0,0 +1,33 @@
+function sendMail(){
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let message = document.getElementById('message').value;
+    var json = {
+        "mailContent": `<h2>이름(회사명) : ${name} </h2><br>
+                    메일 : ${email}<br>
+                    연락처 : ${phone}<br>
+                    ----문의사항----<br>
+                    ${message}`
+    };
+
+    let submit = fetch('http://localhost:3000/sendMail', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(json)
+    })
+    .then(response => {
+        console.log("response => ",response);
+        return response.json();
+    })
+    .catch(error => {
+        console.log("error => ", error);
+    })
+    .then(data => {
+        console.log("data => ", data);
+        document.getElementById("mailResult").textContent = data.message;
+    })
+}
---
+++ summit/routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+router.use(bodyParser.json());
+router.use(express.urlencoded({ extended: true }));
+
+/**메인 */
+router.get('/', function(req, res, next) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+/**메일 전송 */
+router.post('/sendMail', async function(req, res, next) {
+  var json = req.body;
+  console.log(json.mailContent);
+
+  try{
+    const transport = nodemailer.createTransport({
+      host: 'smtp.daum.net',
+      port: 465,
+      secure: true,
+      // 이메일을 보낼 계정 데이터 입력
+      auth: { 
+        user: 'summit.kr',
+        pass: 'sec!8814307!'
+      },
+      from: 'summit@summitkr.com'
+    });
+  
+    // 옵션값 설정
+    const options = {
+      from: 'summit@summitkr.com',
+      to: 'summit4307@gmail.com',
+      subject: '홈페이지 메일 문의 건 입니다',
+      html: json.mailContent,
+    };
+  
+    let send = await transport.sendMail(options);
+    console.log(`Message Sent: `, options);
+    return res.send({"message": '메일이 전송되었습니다' });
+  }catch (err) {
+    console.log(err);
+    return res.send({"message": '메일 전송에 실패했습니다. 다시 시도해주세요' });
+  }
+}); 
+
+module.exports = router;

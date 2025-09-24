f787c640aa6f8a76b2d6594292ba9774feaeebf2
웅경 <jek01680@naver.com>
2025-09-24 14:00:14 +0900

    fix

--- summit/app.js
+++ summit/app.js
@@ -29,16 +29,9 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.locals.title = "오류 발생";
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('서버 에러가 발생했습니다.');
 });
 
 module.exports = app;
